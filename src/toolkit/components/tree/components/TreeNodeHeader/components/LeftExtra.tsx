f05644d2e87a07f349142b32d3718f095ea9e386
czmac <wp412542@alibaba-inc.com>
2024-09-12 15:08:36 +0800

    update somthing

--- src/toolkit/components/tree/components/TreeNodeHeader/components/LeftExtra.tsx
+++ src/toolkit/components/tree/components/TreeNodeHeader/components/LeftExtra.tsx
@@ -1,9 +1,9 @@
 import { Box } from "@chakra-ui/react";
 
 export const LeftExtra = ({ viewState, viewSystem }) => {
-    if (viewState.loading) {
-      return viewSystem.render("icon-loading");
-    }
+    // if (viewState.loading) {
+    //   return viewSystem.render("icon-loading");
+    // }
     if (viewState.expandable) {
       return viewState.expanded
         ? viewSystem.render("icon-expanded")
