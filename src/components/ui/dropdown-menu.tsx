9484057e0f2193b024f18723ae238f43a77e546e
Peiiii <wangpeiiii618@gmail.com>
2025-11-14 01:20:43 +0800

    Prevent auto-focus on trigger button when DropdownMenu closes

--- src/components/ui/dropdown-menu.tsx
+++ src/components/ui/dropdown-menu.tsx
@@ -59,7 +59,7 @@ DropdownMenuSubContent.displayName =
 const DropdownMenuContent = React.forwardRef<
   React.ElementRef<typeof DropdownMenuPrimitive.Content>,
   React.ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.Content>
->(({ className, sideOffset = 4, ...props }, ref) => (
+>(({ className, sideOffset = 4, onCloseAutoFocus, ...props }, ref) => (
   <DropdownMenuPrimitive.Portal>
     <DropdownMenuPrimitive.Content
       ref={ref}
@@ -68,6 +68,11 @@ const DropdownMenuContent = React.forwardRef<
         "z-[9999] min-w-[8rem] overflow-hidden rounded-md border bg-popover p-1 text-popover-foreground shadow-md data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
         className
       )}
+      onCloseAutoFocus={(event) => {
+        // Prevent auto-focus on trigger button when dropdown closes
+        event.preventDefault();
+        onCloseAutoFocus?.(event);
+      }}
       {...props}
     />
   </DropdownMenuPrimitive.Portal>
